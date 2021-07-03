68d2699fcf4eab143db661674112a37728b8c1ac
kjaeschwartz <71410588+kjaeschwartz@users.noreply.github.com>
2021-07-03 09:35:22 -0700

    Update state-compliance.py
    updated program

--- state-compliance.py
+++ state-compliance.py
@@ -1,126 +1,108 @@
-# from datetime import datetime
-# import sys
+from datetime import datetime
+#you know i might be able to just plug data into an excel spreadsheet and it can spit out things back for me.
 #TODO:// MAKE STATE FUNCTION THAT GETS THE STATE AND CONVERTS IT INTO A PROPER FORMAT
 
-state = input("State:")
+state = input("Enter the state (be careful of spelling - im too lazy at the moment to do something that can handle mispellings)\n Side note 2: if the state is in the US virgin islands just type VI or US Virgin islands:\n: ")
 
-        
 #global variables, these should work fine inside of my functions
 yes = ["Y","y","Yes","yes","YES"]
 no = ["N","n","No","no","NO"]
 
-norulesapply = "No state-specific rules apply in your case. yay!"
-notrelevant = "'s rules arent relevant to you! yay!"
-staterules = state + " Rules\n---------------------------------------------------------------------------------------------"
 #hmm should i put the state lists in here or in a constant. eh right now its easier to put them in here
 def get_state(state):
     state = state.upper()
     state_restrictions_short = ['CA', 'DC', 'GA', 'HI', 'IN', 'KS', 'KY', 'MD','MA', 'MI', 'MT', 'NH', 'NM', 'NY', 'PA', 'VI', 'WA', 'WI']
     st_restrictions = ['CALIFORNIA', 'DISTRICT OF COLUMBIA', 'GEORGIA', 'HAWAII', 'INDIANA', 'KANSAS', 'KENTUCKY', 'MARYLAND', 'MASSACHUSETTS',
     'MICHIGAN', 'MONTANA', 'NEW HAMPSHIRE', 'NEW MEXICO', 'NEW YORK', 'PENNSYLVANIA', 'U.S. VIRGIN ISLANDS', 'WASHINGTON', 'WISCONSIN']
+    
     #makes state(now capitalized) into its state abbreviation
-    if state in state_restrictions_short:
-        return state
     if state in st_restrictions:
         index = st_restrictions.index(state)
         state = state_restrictions_short[index]
-        return state
         #state is now in correct format now i turn it into a function call and pass it too my switcher function
-    else:
-        print("This state has no restrictions - program quitting, feel free to restart")
-        sys.exit()
+    return state
         #this function might be out of scope but i can figure it out later
         
-def pos_loc():
-    while True:
-        threenums = [1,2,3]
-        pos_locat = input(f"Is (1) the position location in {state},(2) the address location in {state} \nor (3) are both the position and the address in {state}:\n")
-        pos_locat = int(pos_locat)
-        if int(pos_locat) in threenums:
-            return pos_locat
-        else:
-            print("Input 1,2 or 3")
-
-
+        
 # for i in range(0,len(st_restrictions)):
 #     st_restrictions[i] = state_restrictions_short[i]
 # #makes st_restrictions == state_restrictiosn sh0rt- effectively turning all long versions of states into abbreviations    
-
+    
 
 def CA():
     #tests position location rules -raises enter1or2 if invalid response
-    SF = input("Is the position in San Francisco?:")
-    print("\n\n")
-    if int(pos_location) == 1:
-        print(staterules)
-        print("Do not report dismeanor marijuana cases outside of a 2 year scope")
-        #maybe put in something later where i can just ask questions about whether its a misdemeanor marijuana or not and if the answer is no i cam move on
-        #eventually it would be great to get to a point where i can just have people enter info in a certain format, or fill in a form of some type and then
-        #i can spit out the correct answer
-        # break
-    elif int(pos_location) == 2:
-        print(staterules)
-        print("Rule 1: Only report convictions and active pending cases / warrants (do not report non-convictions / alternates \n(deferrals / diversions in which individual is on probation)")
-        print("Rule 2: Do not report out of 7 year scope")
-        print("Rule 3:  Do not report 1203.4 case status or dispositions")
-        # break
-    elif int(pos_location) == 3:
-        print(staterules)
-        print("Rule 1: Only report convictions and active pending cases / warrants (do not report non-convictions / alternates, (deferrals / diversions in which individual is on probation))")
-        print("Rule 2: Do not report out of 7 year scope.")
-        print("Rule 3: Do not report 1203.4 case status or dispositions.")
-        print("Rule 4: Do not report dismeanor marijuana cases outside of a 2 year scope.")
-        # break
-        # break
-        # else:
-        #     print("Enter 1, 2, or 3!")
-        #     pos_location = input("Is (1) the position location in california,(2) the address location in California \n or (3) are both the position and the address in California \n")
+    SF = input("Is the position in San Francisco?: ")
+    while True:
+        pos_location = input("Is (1) the position location in california,(2) the address location in California \n or (3) are both the position and the address in California \n")
+        if pos_location == 1:
+            print("Do not report dismeanor marijuana cases outside of a 2 year scope")
+            #maybe put in something later where i can just ask questions about whether its a misdemeanor marijuana or not and if the answer is no i cam move on
+            #eventually it would be great to get to a point where i can just have people enter info in a certain format, or fill in a form of some type and then
+            #i can spit out the correct answer
+            break
+        elif pos_location == 2:
+            print("Rule 1: \n Only report convictions and active pending cases / warrants (do not report non-convictions / alternates \n (deferrals / diversions in which individual is on probation))\n")
+            print("Rule 2: \n Do not report out of 7 year scope\n")
+            print("Rule 3: \n Do not report 1203.4 case status or dispositions")
+            break
+        elif pos_location == 3:
+            print("Rule 1: \n Only report convictions and active pending cases / warrants (do not report non-convictions / alternates \n (deferrals / diversions in which individual is on probation))\n")
+            print("Rule 2: \n Do not report out of 7 year scope\n")
+            print("Rule 3: \n Do not report 1203.4 case status or dispositions\n")
+            print("Rule 4: \n Do not report dismeanor marijuana cases outside of a 2 year scope")
+            break
+
+        else:
+            print("Enter 1, 2, or 3!")
+            pos_location = input("Is (1) the position location in california,(2) the address location in California \n or (3) are both the position and the address in California \n")
     # tests and instructs for the marijuana scope rule    -raises customError if invalid response
     while True:
         if SF in yes:
             marijuana = input("Is the disposition a non-felony marijiana conviction?: ")
-            if marijuana in yes:
+            if marijuana in no:
                 print("Do not report non-felony marijuana convictions -  Exception: Report if the disposition is within the past 2 \n years and the individual was under age 21 at the time of conviction")
             else:
                 print("Continue on")
             break
         elif SF in no:
+        
             break
         else:
             print("Enter Y or N!")
     return
 def DC(): 
-    if pos_location == 1 or pos_location == 3:
-        print(f"{state} Rules:\n Do not report out of a 10 year scope")
-        return
+    print("If position is in DC do not report out of a 10 year scope")
+    return
 def GA():
-    print(f"{state} Rules:\n-Do not report First Offender Act case status or disposition")
+    print("Do not report First Offender Act case status or disposition")
     return
 def HI():
-    if pos_location == 1 or pos_location == 3:
-        print(f"{state} Rules:")
-        print("Rule 1: Only report convictions and active pending cases / warrants (do not report non-convictions / alternates \n(deferrals / diversions in which individual is on probation))\n")
-        print("Rule 2: Do not report FELONY charges outside 7 year (may only use incarceration for the 7 years scope - \n NOT probation or parole - otherwise use disposition date)\n ")
-        print("Rule 3: Do not report: MISDEMEANOR charges outside 5 year (may only use incarceration for the 5 year \n scope - NOT probation or parole - otherwise use disposition date)\n")
-    elif pos_location == 2:
-        print(f"{state} Rules:")
-        print(norulesapply)
+    while True:
+        pos_loc = input("Is the position location in Hawaii?: ")
+        if pos_loc in ("Y","y","Yes","yes","YES"):
+            print("Rule 1: Only report convictions and active pending cases / warrants (do not report non-convictions / alternates \n(deferrals / diversions in which individual is on probation))\n")
+            print("Rule 2: Do not report FELONY charges outside 7 year (may only use incarceration for the 7 years scope - \n NOT probation or parole - otherwise use disposition date)\n ")
+            print("Rule 3: Do not report: MISDEMEANOR charges outside 5 year (may only use incarceration for the 5 year \n scope - NOT probation or parole - otherwise use disposition date)\n")
+            break
+        elif pos_loc in ("N","n","No","no","NO"):
+            print("Dont worry about any rules then!")
+            break
+        else:
+            print("Enter Y or N")
     return
 def IN():
     while True:
         residenceIN = input("Is the candidates residence in Indiana?: ")
-        if residenceIN in yes:
-            print(f"{state} Rules:")
+        if residenceIN in ("Y","y","Yes","yes","YES"):
             print("Do not report Class D felony convictions that have been entered or converted to Class A \n misdemeanor convictions, only report the misdemeanor")
             break
-        elif residenceIN in no:
+        elif residenceIN in ("N","n","No","no","NO"):
             print("This rule isnt relevant to you!")
             break
         else:
             print("Enter Y or N")
     return
 def KS():
-    print(f"{state} Rules:")
     print("Do not report out of 7 year scope, unless candidate makes $20,000 or more annually")
     return
 def KY():
@@ -130,14 +112,15 @@ def KY():
             print("Only report convictions and warrants (do not report pending or alternate \n(diversion, dismissed, or any related dispositions))\n")
             break
         elif V in no:
-            print(state + notrelevant)
+            print("Continue on!")
             break
         else:
             print("Enter Y or N")
     return
 def MD():
     while True:
-        if pos_location == 1 or pos_location == 3:
+        V = input("Was the position in Montogomery County MD?:")
+        if V in ("Y","y","Yes","yes","YES"):
             print("Do not report misdemeanor conviction if disposition date is more than 3 years.")
             V1 = input("Was the offense also in maryland only?")
             if V1 in ("Y","y","Yes","yes","YES"):
@@ -155,22 +138,13 @@ def MA():
     print("Do not report outside of 7 year scope")
     return
 def MI():
-    if pos_location == 1 or pos_location == 3:
-        print("Do not report misdemeanor alternate disposition (e.g. diversion, deferral, ...)")
-    else:
-        print(state + " " + notrelevant)
+    print("If the position is in Michigan do not report misdemeanor alternate disposition (e.g. diversion, deferral, ...)")
     return
 def MT():
-    if pos_location == 2 or pos_location == 3:
-        print("Do not report out of a 7 year scope.")
-    else:
-        print(state + " " + notrelevant)
+    print("If the residency is in Montana do not report out of a 7 year scope.")
     return
 def NH():
-    if pos_location == 2 or pos_location == 3:
-        print("Do not report out of 7 year scope, unless candidate makes $20,000\n or more annually")
-    else:
-        print(state + " " + notrelevant)
+    print("If the residency is in New Hampshire do not report out of 7 year scope, unless candidate makes $20,000\n or more annually")
     return
 def NM():
     print("If residency is in New Mexico:")
@@ -178,32 +152,27 @@ def NM():
     print("Rule 2: Do not report outside of a 7 year scope.")
     return
 def NY():
-    print(staterules)
-    print("Rule 1: Only report convictions and active pending cases / warrants (do not report non-convictions \n / alternates (deferrals / diversions in which individual is on probation))")
-    print("Rule 2: Do not report Adjournment in contemplation of dismissal (ACD) disposition)")
-    print("Rule 3:Do not report out of 7 year scope, unless candidate makes $25,000 or more annually")
+        print("Rule 1: Only report convictions and active pending cases / warrants (do not report non-convictions \n / alternates (deferrals / diversions in which individual is on probation))")
+        print("Rule 2: Do not report Adjournment in contemplation of dismissal (ACD) disposition)")
+        print("Rule 3:Do not report out of 7 year scope, unless candidate makes $25,000 or more annually")
 def PA():
     posloc == True
     #placeholder for when later I'll probably parameterize the position location/ residency as a constant or global var so 
     #users dont have to specify every time. I can just as a question as posR"= ()
-    if pos_location == 1 or pos_location == 3:
+    if posloc == True:
         print("If the residency is in Pennsylvania only report convictions and warrents \n(do not report pending or alternate (diversion, dismissed, or any related dispositions))")
 def VI():
-    if pos_location == 2 or pos_location == 3:
-        print("Only report convictions and warrants (do not report pending or alternate \n (diversion, dismissed, or any related dispositions))")
-    else:
-        print(state + " " + notrelevant)
+    print("If residency is in the US Virgin Islands only report convictions and warrants (do not report pending or alternate \n (diversion, dismissed, or any related dispositions))")
     return
 def WA():
-    if pos_location == 2 or pos_location == 3:
-        print("If the makes >$20,000 annually do not report out of 7 year scope")
+    print("If the candidate has residency in Washington and makes >$20,000 annually do not report out of 7 year scope")
     return
     #TODO edit this later so i can input the pos_res variable into the function def
 def WI():
     Madison = input("Is the position in Madison, WI? (enter Y/N):")
-    if Madison in yes:
+    if Madison in ("Y","y"):
         ("Only report 3 years from when the individual has been placed on probation (start date) / disposition \ndate, paroled (start of parole), released from incarceration, or paid a fine")
-    if pos_location == 2 or pos_location == 3:
+    if pos_res in ("R","r","B","b"):
         print("Do not report Guilty But Not Guilty Due to Mental Disease/Defect disposition")
         
         
@@ -240,9 +209,9 @@ def state_cases(state):
     # Get the function from switcher dictionary
     Switch_func = state_switcher.get(state, lambda: "No special restrictions on entered state.")
     # Execute the function
-    Switch_func()
+    print(Switch_func())
 
+pos_res = input("Is (P) the position location in the state you entered, (R) the residency location in the state you entered or (B) both?:")
 #the statement above is for in the future where I start only listing rules depending on whether pos_res is.
-state = get_state(state)
-pos_location = pos_loc()
+get_state(state)
 state_cases(state)
